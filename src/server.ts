6cb731b80119e9dfc20705f84a795bc45815a1ce
Magdiel09 <magdiel092013@gmail.com>
2021-07-22 14:23:34 -0400

    v2 29:28

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,39 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response)=> {
-    console.log('Listagem de usuários!');
-    
-    response.json([
+app.use(express.json());
+
+const users = [
         'Diego',
-        'Magdiel',
-        'Robson',
-        'Rayssa',
-        'Jorge',
-        'Lucas'
-    ]);
+        'Carla',
+        'Lucas',
+        'Daniel'
+];
+
+app.get('/users', (request, response)=> {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email 
+    }
+
+    return response.json(user);
 });
 
 app.listen(3333);
