9f77979291cdb5d2341ff09878e304db308db023
magdielmarques <contato.magdiel@gmail.com>
2021-07-28 11:26:12 -0400

    fim do dia 2

--- src/controllers/PointsControllers.ts
+++ src/controllers/PointsControllers.ts
@@ -2,16 +2,45 @@ import {Request, Response} from 'express';
 import knex from "../database/connection";
 
 class PointsController {
+    async index (request: Request, response: Response) {
+        const { city, uf, items } = request.query;
+
+        const parsedItems = String(items)
+        .split(',')
+        .map(item => Number(item.trim()));
+
+        const points = await knex('points')
+            .join('point_items', 'points.id', '=', 'point_items.point_id')
+            .whereIn('point_items.item_id', parsedItems)
+            .where('city', String(city))
+            .where('uf', String(uf))
+            .distinct()
+            .select('points.*');
+
+        response.json(points);
+    }
+
     async show (request: Request, response: Response) {
         const id = request.params.id
         //const { id } = request.parms;
         const point = await knex('points').where('id', id).first();
 
+        /*
+        SELECT * FROM items
+            JOIN point_items ON items.id = point_items.item_id
+            WHERE point_items.point_id = {id}
+        
+        */
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item_id')
+            .where('point_items.point_id', id)
+            .select('items.title');
+
         if(!point) {
             return response.status(400).json({ message: 'Point not found.' })
         }
 
-        return  response.json(point);
+        return  response.json({ point, items });
     }
 
     async create (request: Request, response: Response) {
@@ -31,7 +60,7 @@ class PointsController {
         } = request.body;
     
         const point = {
-            image: 'image-fake',
+            image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
@@ -42,9 +71,9 @@ class PointsController {
             /*name: name não é necessário com short sintax, quando o nome da váriavel é igual ao nome da propriedade do objeto*/
         };
 
-        //const trx = await knex.transaction();
+        const trx = await knex.transaction();
         
-        const insertedIds = await knex('points').insert(point);
+        const insertedIds = await trx('points').insert(point);
         
         const  point_id = insertedIds[0] ;
 
@@ -55,7 +84,9 @@ class PointsController {
             };
         });
     
-        await knex('point_items').insert(pointItems);
+        await trx('point_items').insert(pointItems);
+
+        await trx.commit();
     
         return response.json({
             id: point_id,
